06e5f974eaf09c09585275a027627e95ea1dc113
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-07 12:48:57 -0300

    Time: 43 ms (50.96%), Space: 13.9 MB (45.37%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,20 +1,19 @@
 class Solution:
+    def __init__(self):
+        self.calculated = {}
+        
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        value = self.maxValue(nums, 0, len(nums)-1)
-        # print(value)
-        return value >= 0
-    
-    # def value(self, nums, l, r, turn):
-    #     # maximizer turn = 0
-    #     # minimizer turn = 1
-    #     if l==r: return nums[l]
-    #     if turn == 0: return self.minValue(nums, l, r)
-    #     if turn == 1: return self.maxValue(nums, l, r)
+        print(self.maxValue(nums, 0, len(nums)-1))
+        return self.maxValue(nums, 0, len(nums)-1) >= 0
     
     def maxValue(self, nums, l, r):
         if l==r: return nums[l]
-        leftValue = nums[l] + self.minValue(nums, l+1, r)
-        rightValue = nums[r] + self.minValue(nums, l, r-1)
+        if 'min({l}, {r})'.format(l = l+1, r = r) not in self.calculated:
+            self.calculated['min({l}, {r})'.format(l = l+1, r = r)] = self.minValue(nums, l+1, r)
+        if 'min({l}, {r})'.format(l = l, r = r-1) not in self.calculated:
+            self.calculated['min({l}, {r})'.format(l = l, r = r-1)] = self.minValue(nums, l, r-1)
+        leftValue = nums[l] + self.calculated['min({l}, {r})'.format(l = l+1, r = r)]
+        rightValue = nums[r] + self.calculated['min({l}, {r})'.format(l = l, r = r-1)]
         if leftValue > rightValue:
             return leftValue
         else:
@@ -22,8 +21,12 @@ class Solution:
         
     def minValue(self, nums, l, r):
         if l==r: return nums[l]
-        leftValue = self.maxValue(nums, l+1, r) - nums[l]
-        rightValue = self.maxValue(nums, l, r-1) -nums[r]
+        if 'max({l}, {r})'.format(l = l+1, r = r) not in self.calculated:
+            self.calculated['max({l}, {r})'.format(l = l+1, r = r)] = self.maxValue(nums, l+1, r)
+        if 'max({l}, {r})'.format(l = l,r = r-1) not in self.calculated:
+            self.calculated['max({l}, {r})'.format(l = l,r = r-1)] = self.maxValue(nums, l, r-1)
+        leftValue = self.calculated['max({l}, {r})'.format(l = l+1, r = r)] - nums[l]
+        rightValue = self.calculated['max({l}, {r})'.format(l = l,r = r-1)] - nums[r]
         if leftValue > rightValue:
             return rightValue
         else:
