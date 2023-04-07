4f1e1efcbb9bcc192e3e9ceaf597b746cc9b756b
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-07 12:19:05 -0300

    Time: 3631 ms (31.98%), Space: 13.8 MB (98.09%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,18 +1,20 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        return self.maxValue(nums, 0, len(nums)-1) >= 0
+        value = self.maxValue(nums, 0, len(nums)-1)
+        # print(value)
+        return value >= 0
     
-    def value(self, nums, l, r, turn):
-        # maximizer turn = 0
-        # minimizer turn = 1
-        if l==r: return nums[l]
-        if turn == 0: return self.minValue(nums, l, r)
-        if turn == 1: return self.maxValue(nums, l, r)
+    # def value(self, nums, l, r, turn):
+    #     # maximizer turn = 0
+    #     # minimizer turn = 1
+    #     if l==r: return nums[l]
+    #     if turn == 0: return self.minValue(nums, l, r)
+    #     if turn == 1: return self.maxValue(nums, l, r)
     
     def maxValue(self, nums, l, r):
         if l==r: return nums[l]
-        leftValue = nums[l] - self.value(nums, l+1, r, 1)
-        rightValue = nums[r] - self.value(nums, l, r-1, 1)
+        leftValue = nums[l] + self.minValue(nums, l+1, r)
+        rightValue = nums[r] + self.minValue(nums, l, r-1)
         if leftValue > rightValue:
             return leftValue
         else:
@@ -20,8 +22,8 @@ class Solution:
         
     def minValue(self, nums, l, r):
         if l==r: return nums[l]
-        leftValue = self.value(nums, l+1, r, 0) - nums[l]
-        rightValue = self.value(nums, l, r-1, 0) -nums[r]
+        leftValue = self.maxValue(nums, l+1, r) - nums[l]
+        rightValue = self.maxValue(nums, l, r-1) -nums[r]
         if leftValue > rightValue:
             return rightValue
         else:
