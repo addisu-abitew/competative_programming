259f8f2c6cc638ae90e8697dddd5a5568c502180
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-08 17:30:23 -0300

    Time: 40 ms (35.90%), Space: 13.8 MB (96.84%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        try:
+            if s == 'inf' or s =='-inf' or s == '+inf' or s=="Infinity" or s=="+Infinity" or s=="-Infinity": return False
+            float(s)
+            return True
+        except:
+            return False
