f4ecff647560f137bd6d5cc6f7c09a8fd30c407a
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-15 08:11:20 -0300

    Time: 99 ms (77.54%), Space: 18.3 MB (35.49%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,38 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0: return
-        if len(lists) == 1: return lists[0]
-        min_i = -1
-        # initialize min_i by the first non-null list head value
-        for li in range(len(lists)):
-            if lists[li]:
-                min_i = li
-                break
-        if min_i == -1: return None
-        
-        # find the smallest value
-        for i in range(len(lists)):
-            if lists[i] and lists[i].val<lists[min_i].val:
-                min_i = i
-        
-        sorted_list = ListNode(lists[min_i].val)
-        lists[min_i] = lists[min_i].next
+        vals = []
+        for l in lists:
+            while l:
+                vals.append(l.val)
+                l = l.next
+        if len(vals)==0: return
+        vals.sort()
+        sorted_list = ListNode(vals[0])
         last = sorted_list
-        
-        while True:
-            min_i = -1
-            for li in range(len(lists)):
-                if lists[li]:
-                    min_i = li
-                    break
-            if min_i == -1: break
-            
-            for i in range(len(lists)):
-                if lists[i] and lists[i].val<lists[min_i].val:
-                    min_i = i
-            last.next = ListNode(lists[min_i].val)
-            lists[min_i] = lists[min_i].next
-            last = last.next           
-        
+        for i in range(1, len(vals)):
+            last.next = ListNode(vals[i])
+            last = last.next
         return sorted_list
