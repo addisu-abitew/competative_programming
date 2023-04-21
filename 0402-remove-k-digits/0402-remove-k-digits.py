d86758866b6e37a6a59e9add351d5c1265a028b2
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-21 18:43:24 -0300

    Time: 388 ms (5.04%), Space: 15.3 MB (94.70%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -2,18 +2,31 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         if len(num) == k:
             return "0"
-        stack = []
-        for n in num :
-            while stack and int(stack[-1]) > int(n) and k:
-                stack.pop()
-                k -= 1
-            stack.append(n)
-            
-        while k:
-            stack.pop()
-            k -= 1
-        return str(int(''.join(stack)))
+        nums = list(num)
+        removed = 0
+        i ,p = 0 ,-1
+        while i < len(nums):
+            while i > 0 and nums[i] <= nums[p]:
+                tmp = i
+                while tmp < len(nums) and nums[tmp] == nums[p]:
+                    tmp += 1
                 
-        
-                
-        
+                if tmp == len(nums): 
+                    nums = nums[:len(nums)-(k-removed)]
+                    return str(int(''.join(nums)))
+                elif nums[tmp] < nums[p]:
+                    r = min(tmp-p, k-removed)
+                    nums = nums[:p] + nums[p+r:]
+                    removed += r
+                    if removed == k:
+                        return str(int(''.join(nums)))
+                    i -= 1
+                    p -=  1
+                else:
+                    i = tmp
+                    p = i -1
+                    break
+            p = i
+            i += 1
+        nums = nums[:len(nums)-(k-removed)]
+        return str(int(''.join(nums)))
