bcfec8eb35211dc36dd89013b9744e22e75709b0
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-23 07:58:31 -0300

    Time: 2009 ms (5.02%), Space: 34.2 MB (29.39%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -13,19 +13,17 @@ class Solution:
             self.calculated['min({l}, {r})'.format(l = l, r = r-1)] = self.minValue(piles, l, r-1)
         leftValue = piles[l] + self.calculated['min({l}, {r})'.format(l = l+1, r = r)]
         rightValue = piles[r] + self.calculated['min({l}, {r})'.format(l = l, r = r-1)]
-        if leftValue > rightValue:
-            return leftValue
-        else:
-            return rightValue
+
+        return max(leftValue, rightValue)
         
     def minValue(self, piles, l, r):
-        if l==r: return piles[l]
+        if l==r: return 0
         if 'max({l}, {r})'.format(l = l+1, r = r) not in self.calculated:
             self.calculated['max({l}, {r})'.format(l = l+1, r = r)] = self.maxValue(piles, l+1, r)
         if 'max({l}, {r})'.format(l = l,r = r-1) not in self.calculated:
             self.calculated['max({l}, {r})'.format(l = l,r = r-1)] = self.maxValue(piles, l, r-1)
-        leftValue = self.calculated['max({l}, {r})'.format(l = l+1, r = r)] - piles[l]
-        rightValue = self.calculated['max({l}, {r})'.format(l = l,r = r-1)] - piles[r]
+        leftValue = self.calculated['max({l}, {r})'.format(l = l+1, r = r)]
+        rightValue = self.calculated['max({l}, {r})'.format(l = l,r = r-1)]
         if leftValue > rightValue:
             return rightValue
         else:
