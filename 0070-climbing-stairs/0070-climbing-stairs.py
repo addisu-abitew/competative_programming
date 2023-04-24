88bbf7b39f1a5a0c1c6a008a206db906ed14b8e0
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-24 18:39:45 -0300

    Time: 35 ms (29.59%), Space: 13.9 MB (6.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,10 +2,7 @@ from math import factorial
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n<4: return n
-        res = 0
-        for twos in range(n//2, -1, -1):
-            ones = n - 2*twos
-            div = factorial(ones) * factorial(twos)
-            res += factorial(ones+twos)//div
-        return res
+        down, up = 1, 1
+        for i in range(n-1):
+            up, down = up+down, up
+        return up
