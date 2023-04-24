63666f74445af7fc2fa53cc44dc1d038ad038efb
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-04-24 19:08:59 -0300

    Time: 58 ms (72.52%), Space: 14 MB (65.88%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,6 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int], i = -1, mem={}) -> int:
-        if i+2 >= len(cost):
-            mem[i] = 0
-            return 0
-        if i+1 not in mem: mem[i+1] = self.minCostClimbingStairs(cost, i+1, mem)
-        if i+2 not in mem: mem[i+2] = self.minCostClimbingStairs(cost, i+2, mem)
-        if i not in mem: mem[i] = min(cost[i+1]+mem[i+1], cost[i+2]+mem[i+2])
-        res = mem[i]
-        if i == -1: mem.clear()
-        return res
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(len(cost)-3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
