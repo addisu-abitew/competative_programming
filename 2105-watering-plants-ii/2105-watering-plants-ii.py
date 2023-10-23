983a9e3503b883be7e77fd4f69e6de7df465519a
abitewaddisu <104774854+abitewaddisu@users.noreply.github.com>
2023-10-23 16:43:38 -0300

    Time: 682 ms (83.04%), Space: 31.2 MB (99.42%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.py
+++ 2105-watering-plants-ii/2105-watering-plants-ii.py
@@ -3,36 +3,19 @@ class Solution:
         l, r = 0, len(plants)-1
         fill = 0
         curA, curB = capacityA, capacityB
-        while l <= r:
-            if l == r:
-                if curA >= curB:
-                    cur = plants[l]
-                    if curA < cur:
-                        fill += 1
-                        curA = capacityA - cur
-                    else:
-                        curA -= cur
-                elif curA < curB:
-                    cur = plants[r]
-                    if curB < cur:
-                        fill += 1
-                        curB = capacityB - cur
-                    else:
-                        curB -= cur
-            else:
-                curL, curR = plants[l], plants[r]
-                
-                if curA < curL:
-                    fill += 1
-                    curA = capacityA - curL
-                else:
-                    curA -= curL
-                    
-                if curB < curR:
-                    fill += 1
-                    curB = capacityB - curR
-                else:
-                    curB -= curR
+        while l < r:
+            curL, curR = plants[l], plants[r]
+            if curA < curL:
+                fill += 1
+                curA = capacityA
+            if curB < curR:
+                fill += 1
+                curB = capacityB
+            curA -= curL
+            curB -= curR
             l += 1
             r -= 1
+        
+        if l == r and (max(curA, curB) < plants[l]):
+            fill += 1
         return fill
