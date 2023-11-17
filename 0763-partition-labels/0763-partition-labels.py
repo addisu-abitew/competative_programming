26e5bb7c8451f7345a5892faf39af28b358fd746
Addisu Abitew <104774854+abitewaddisu@users.noreply.github.com>
2023-11-17 09:02:10 -0300

    Time: 51 ms (24.72%), Space: 16.4 MB (8.26%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,14 +1,17 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        dic = {}
-        for i, l in enumerate(s):
-            dic[l] = i
-        
-        ans = []
-        l, r = -1, 0
+        mem = {}
         for i in range(len(s)):
-            r = max(r, dic[s[i]])
-            if i == r:
-                ans.append(r-l)
-                l = r
+            mem[s[i]] = i
+        l = -1
+        r = 0
+        ans = []
+        while r < len(s):
+            r = mem[s[r]]
+            init_l = l
+            while l < r:
+                r = max(r, mem[s[l+1]])
+                l += 1
+            ans.append(mem[s[r]]-init_l)
+            r += 1
         return ans
