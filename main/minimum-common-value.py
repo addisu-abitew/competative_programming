6d8b8306b5e2d2064ebbcee0c7b38c6e320a92bb
Addisu Abitew <104774854+abitewaddisu@users.noreply.github.com>
2024-01-03 12:09:54 -0300

    Added solution for Minimum Common Value

---
+++ main/minimum-common-value.py
@@ -0,0 +1,4 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        intersection = set(nums1) & set(nums2)
+        return min(intersection) if intersection else -1
