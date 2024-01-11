3b02a7ffc5276809fee17b318c48d2615f48ed72
Addisu Abitew <104774854+abitewaddisu@users.noreply.github.com>
2024-01-11 12:53:29 -0300

    Added solution for Sort Colors

--- main/sort-colors.py
+++ main/sort-colors.py
@@ -1,7 +1,11 @@
-class Solution(object):
-    def sortColors(self, nums):
-        for i in range(len(nums)-1):
-            for j in range(len(nums)-i-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-        return nums
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = Counter(nums)
+        i = 0
+        for num in range(3):
+            for _ in range(count[num]):
+                nums[i] = num
+                i += 1
