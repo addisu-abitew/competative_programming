2ec27603c6332362aa46b3256e052733e7324264
Addisu Abitew <104774854+addisu-abitew@users.noreply.github.com>
2025-09-22 09:24:51 +0300

    Time: 1 ms (48.29%), Space: 17.7 MB (86.68%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
@@ -2,17 +2,15 @@ class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         # count the number of occurence of each number
         freq = {}
+        max_count, res = 0, 0
         for num in nums:
             if num not in freq:
                 freq[num] = 0
             freq[num] += 1
-        
-        max_count = 0
-        res = 0
-        for val in freq:
-            if freq[val] > max_count:
-                max_count = freq[val]
+            if freq[num] > max_count:
+                max_count = freq[num]
                 res = max_count
-            elif freq[val] == max_count:
+            elif freq[num] == max_count:
                 res += max_count
+        
         return res
